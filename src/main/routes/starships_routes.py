1f854660dcbdf15ae05b4642fc480bfd1a46716a
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-11-29 23:16:31 -0300

    feat: implements http verification with cerberus

--- src/main/routes/starships_routes.py
+++ src/main/routes/starships_routes.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Request as RequestFastApi
+from src.validators.get_starships_in_pagination_validator import get_pagination_validator
 
 starships_routes = APIRouter()
 
@@ -6,4 +7,6 @@ starships_routes = APIRouter()
 @starships_routes.get("/api/starships/list")
 def get_starships_in_pagination(request: RequestFastApi):
     ''' get_starships_in_pagination '''
+
+    get_pagination_validator(request)
     return {"millenium": "falcon"}
