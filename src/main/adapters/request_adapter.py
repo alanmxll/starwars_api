4976d8cae9b6f859720f13f1f847a4ba794eebb3
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-11-30 22:58:36 -0300

    feat: adapting the framework

---
+++ src/main/adapters/request_adapter.py
@@ -0,0 +1,25 @@
+from typing import Callable
+
+from fastapi import Request as RequestFastApi
+
+
+async def request_adapter(request: RequestFastApi, callback: Callable):
+    ''' FastApi Adapter '''
+
+    body = None
+
+    try:
+        body = await request.json()
+    except:
+        pass
+
+    http_request = {
+        "query_params": request.query_params,
+        "body": body
+    }
+
+    try:
+        http_response = callback(http_request)
+        return http_response
+    except:
+        print('An Error Has Been Ocurred.')
--- src/main/routes/starships_routes.py
+++ src/main/routes/starships_routes.py
@@ -1,12 +1,17 @@
-from fastapi import APIRouter, Request as RequestFastApi
-from src.validators.get_starships_in_pagination_validator import get_pagination_validator
+from fastapi import APIRouter
+from fastapi import Request as RequestFastApi
+from src.main.adapters import request_adapter
+from src.validators.get_starships_in_pagination_validator import \
+    get_pagination_validator
 
 starships_routes = APIRouter()
 
 
 @starships_routes.get("/api/starships/list")
-def get_starships_in_pagination(request: RequestFastApi):
+async def get_starships_in_pagination(request: RequestFastApi):
     ''' get_starships_in_pagination '''
 
     get_pagination_validator(request)
+    await request_adapter(request, print)
+
     return {"millenium": "falcon"}
