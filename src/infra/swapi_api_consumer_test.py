1b72fe2be61e8ae2f41c1cea537c660d41015989
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-12-02 21:55:53 -0300

    feat: add new external request

--- src/infra/swapi_api_consumer_test.py
+++ src/infra/swapi_api_consumer_test.py
@@ -44,3 +44,48 @@ def test_get_starships_http_error(requests_mock):
     except HttpRequestError as error:
         assert error.message is not None
         assert error.status_code is not None
+
+
+def test_get_starship_information_response(requests_mock):
+    '''
+    Testing get_starship_information method
+    '''
+    starship_id = 9
+    swapi_api_consumer = SwapiApiConsumer()
+
+    requests_mock.get(
+        f'https://swapi.dev/api/starships/{starship_id}',
+        status_code=200,
+        json={'name': 'some', 'model': 'thing', 'MGLT': '123'}
+    )
+
+    staship_information = swapi_api_consumer.get_starship_information(
+        starship_id)
+
+    assert staship_information.request.method == 'GET'
+    assert staship_information.request.url == f'https://swapi.dev/api/starships/{starship_id}'
+    assert staship_information.status_code == 200
+
+    assert "MGLT" in staship_information.response
+
+
+def test_get_starship_information_error(requests_mock):
+    '''
+    Testing get_starship_information method in error
+    '''
+    starship_id = 1
+    swapi_api_consumer = SwapiApiConsumer()
+
+    requests_mock.get(
+        f'https://swapi.dev/api/starships/{starship_id}',
+        status_code=404,
+        json={'detail': 'something'}
+    )
+
+    try:
+        swapi_api_consumer.get_starship_information(
+            starship_id)
+        assert True is False
+    except HttpRequestError as error:
+        assert error.message is not None
+        assert error.status_code is not None
